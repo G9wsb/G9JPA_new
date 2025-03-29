907f0102564f39818bb3797b59ce9835a6063aa9
G9wsb <wrx95293@student.wroclaw.merito.pl>
2025-03-29 16:14:03 +0100

    Update VisitEntity.java

--- wsb-jpa1/src/main/resources/data.sql
+++ wsb-jpa1/src/main/resources/data.sql
@@ -3,20 +3,20 @@ insert into address (id, address_line1, address_line2, city, postal_code)
 
 -- ADDRESS
 insert into address (id, city, address_line1, address_line2, postal_code)
-values (1, 'Warsaw', 'Main St', 'Apt 1', '00-001');
+values (1, 'Wroclaw', 'Strzegosmska', 'Apt 1', '54-200');
 
 -- DOCTOR
 insert into doctor (id, first_name, last_name, telephone_number, email, doctor_number, specialization, address_id)
-values (1, 'Anna', 'Nowak', '123456789', 'anna@clinic.com', 'DOC001', 'GP', 1);
+values (1, 'Marek', 'Skwarek', '123456789', 'maro_skwaro@gmail.com', 'doctor_01', 'proktolog', 1);
 
 -- PATIENT
 insert into patient (id, first_name, last_name, telephone_number, email, patient_number, date_of_birth, address_id)
-values (1, 'Jan', 'Kowalski', '987654321', 'jan@patient.com', 'PAT001', '1990-01-01', 1);
+values (1, 'Hubert', 'Rozpara', '349565456', 'hubiskubidoo@yahoo.com', 'pat_01', '2001-09-11', 1);
 
 -- MEDICAL_TREATMENT
 insert into medical_treatment (id, description, type)
-values (1, 'General check-up', 'USG');
+values (1, 'General anus assessment', 'USG/Manual');
 
 -- VISIT
 insert into visit (id, description, time, doctor_id, patient_id, treatment_id)
-values (1, 'First visit for check-up', '2025-03-23T10:00:00', 1, 1, 1);
+values (1, 'Control appointment', '2025-03-30T12:22:00', 1, 1, 1);
