e3fd77803d4258fbae2c687dca640f9b4b3350b7
G9wsb <wrx95293@student.wroclaw.merito.pl>
2025-04-15 07:52:51 +0200

    Update PatientDaolmpl.java

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaolmpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaolmpl.java
@@ -5,5 +5,5 @@ import com.jpacourse.persistance.entity.PatientEntity;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao{
+public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
 }
